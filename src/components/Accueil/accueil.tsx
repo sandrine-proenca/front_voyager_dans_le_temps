1579c449bbd9f653e1e1a029a0d3789a238ec74a
sandrine-proenca <sandrine.proenca@sncf.fr>
2023-04-21 10:43:21 +0200

    page accueil

--- src/components/Accueil/accueil.tsx
+++ src/components/Accueil/accueil.tsx
@@ -1,54 +1,43 @@
-
 import './accueil.css'
+
 
 export default function Accueil()
 {
 
     return (
-        <div className='container-fluid container'>
+        <div className='pageAccueil'>
 
             <header className="header">
-                <h1 className="text-start font-light p-1"> Voyager dans le temps </h1>
+                <h1 > Voyager dans le temps </h1>
             </header>
 
             <body className='body'>
+                <div className='presentation'>
+                    <h2>Bonjour !</h2>
+                    <p>Cette application concerne toutes les personnes d'une même famille, proche ou éloignée.</p><br />
+                    <p>Elle permet d'échanger, de découvrir ou de se remémorer des photos de personnes qui nous sont chères !</p><br />
+                    <p>Vous pourez y déposer une photo de vos ailleux ou de votre famille et la commenter.</p><br />
+                    <p>Vous pouvez également visionner toutes les photos mises à disposition et y faire un commentaire.</p><br />
+                    <p>Bon voyage dans le temps !</p>
 
-                <div className="modal" tabIndex={-1}>
-                    <div className="modal-dialog">
-                        <div className="modal-content">
-                            <div className="modal-header">
-                                <h5 className="modal-title">Bonjour !</h5>
-                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div className="modal-body">
-                                <p>Cette application concerne toutes les personnes d’ une même famille, proche ou éloignée.
-                                    Elle permet d’ échanger, de découvrir ou de se remémorer des photos de personnes qui nous sont chères !
-                                    Vous pourez y déposer une photo de vos ailleux ou de votre famille et la commenter.
-                                    Vous pouvez également visionner toutes les photos mises à disposition et y faire un commentaire.
+                    <div className='container text-center'>
+                        <div className='row justify-content-evenly text-center align-items-center mt-5'>
+                            <button type="button" className="btn button btn-color col-5 btn-sm">
+                                inscription
+                            </button>
 
-                                    Bon voyage dans le temps !</p>
-                            </div>
-                            <div className="modal-footer">
-                                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                <button type="button" className="btn btn-primary">Save changes</button>
-                            </div>
+                            <button type="button" className="btn button btn-color col-5 btn-sm">
+                                connexion
+                            </button>
                         </div>
                     </div>
                 </div>
-
-
-                {/* <h2>Bonjour !
-Cette application concerne toutes les personnes d’ une même famille, proche ou éloignée.
-Elle permet d’ échanger, de découvrir ou de se remémorer des photos de personnes qui nous sont chères !
-Vous pourez y déposer une photo de vos ailleux ou de votre famille et la commenter.
-Vous pouvez également visionner toutes les photos mises à disposition et y faire un commentaire.
-
-Bon voyage dans le temps !
-
-
-</h2> */}
-
             </body>
+
+            {/* <footer className='footer'>
+                <h5>Créé par Sandrine Proença  -  Propriété de Sandrine Proença</h5>
+            </footer> */}
+
         </div>
     )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.js';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
