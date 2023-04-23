66d12937f64a86f96452d3836f7bd91c39eb75f0
sandrine-proenca <sandrine.proenca@sncf.fr>
2023-04-23 18:22:53 +0200

    merge album

--- src/components/Album/AlbumNew.tsx
+++ src/components/Album/AlbumNew.tsx
@@ -6,15 +6,16 @@ import { TResponse } from "../../Types/TResponse";
 import { NO_ALBUM } from "../../constant/NoAlbum";
 import { AuthContext } from "../../context/authContext";
 
-export function AlbumNew(props: {
-    albums: TAlbum[],
-    setAlbums: (value: TAlbum[]) => void,
-    setAlbum: (value: TAlbum) => void
-})
+export function AlbumNew(props:
+    {
+        albums: TAlbum[],
+        setAlbums: (value: TAlbum[]) => void,
+        setAlbum: (value: TAlbum) => void
+    })
 {
     const { albums, setAlbums, setAlbum } = props;
     const [ item, setItem ] = useState<TAlbum>(DEFAULT_ALBUM);
-    
+
     const token = useContext(AuthContext).user?.access_token;
 
     const itemHandlerTextuel = (key: "name", value: string) =>
@@ -24,7 +25,8 @@ export function AlbumNew(props: {
         setItem(newItem);
     };
 
-    const createNewAlbum = async () =>{
+    const createNewAlbum = async () =>
+    {
         console.log(item);
         const { id, ...leReste } = item;
         const options = {
@@ -36,32 +38,32 @@ export function AlbumNew(props: {
             body: JSON.stringify(leReste)
         };
 
-        const response = await fetch(`${BASE_URL}albums`, options)
-        const responseJson: TResponse<TAlbum> = await response.json()
+        const response = await fetch(`${BASE_URL}albums`, options);
+        const responseJson: TResponse<TAlbum> = await response.json();
 
         const newAlbums = [ ...albums ];
-            const newAlbum = responseJson.data;
-            newAlbums.push(newAlbum);
-            setAlbums(newAlbums);
+        const newAlbum = responseJson.data;
+        newAlbums.push(newAlbum);
+        setAlbums(newAlbums);
         setAlbum(newAlbum);
 
-        }
-
-        return (
-            <div className="bg-info rounded p-2 m-1">
-                <div>
-                    <label htmlFor="albumName"> Nom de l'album : </label>
-                    <input
-                        name="albumName"
-                        type="text"
-                        defaultValue={item.name}
-                        onChange={(e) => itemHandlerTextuel("name", String(e.target.value))}
-                    />
-                </div>
-                <div>
-                    <button onClick={() => { setAlbum(NO_ALBUM) }}>Annuler la création</button>
-                    <button onClick={createNewAlbum}>Enregister</button>
-                </div>
-                </div>
-                );
     }
+
+    return (
+        <div className="bg-info rounded p-2 m-1">
+            <div>
+                <label htmlFor="albumName"> Nom de l'album : </label>
+                <input
+                    name="albumName"
+                    type="text"
+                    defaultValue={item.name}
+                    onChange={(e) => itemHandlerTextuel("name", String(e.target.value))}
+                />
+            </div>
+            <div>
+                <button onClick={() => { setAlbum(NO_ALBUM) }}>Annuler la création</button>
+                <button onClick={createNewAlbum}>Enregister</button>
+            </div>
+        </div>
+    );
+}
---
+++ src/constant/DefaultPhoto.ts
@@ -0,0 +1,6 @@
+export const DEFAULT_PHOTO = {
+    id: -1,
+    photo: "Nouvelle photo",
+    information: "Nouvelle Information",
+    mimeType: "Nouveau mimeType"
+}
