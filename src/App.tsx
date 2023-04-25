35cb10e5f7cad3f2bb50761f67b40631570f3032
sandrine-proenca <sandrine.proenca@sncf.fr>
2023-04-25 16:42:56 +0200

    ajout app + index

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import Register from './components/Connexion_User/register';
 import Login from './components/Connexion_User/login';
 import Navbar from './components/Navbar/Navbar';
 import { TUser } from './Types/TUser';
+import { CompteUser } from './components/CompteUser/compteUser';
+import UpdateUsers from './components/CompteUser/updateUser';
 
 export default function App() {
 
@@ -23,6 +25,8 @@ export default function App() {
       {page === `accueil` && <Accueil setPage={setPage} page={page}/>}
       {page === `register` && <Register setPage={setPage} page={page}/>}
       {page === `login` && <Login setPage={setPage} page={page}/>}
+      {page === `compteUser` && <CompteUser setPage={setPage}/>}
+      {page === `updateUsers` && <UpdateUsers setPage={setPage}/>}
 
     </AuthContext.Provider>
 
--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,18 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.js';
+import { UserContext, UserContextProvider } from './context/userContext';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-   //render est une méthode qui prend 2 arguments et l'endroit où on va afficher notre appli
-   <BrowserRouter> {/* entouré notre application par BrowserRouter qui nous donne accès aux fonctionnalités de react router */}
-   <App />
- </BrowserRouter>
+  //render est une méthode qui prend 2 arguments et l'endroit où on va afficher notre appli
+  <BrowserRouter> {/* entouré notre application par BrowserRouter qui nous donne accès aux fonctionnalités de react router */}
+    <UserContextProvider>
+      <App />
+    </UserContextProvider>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
