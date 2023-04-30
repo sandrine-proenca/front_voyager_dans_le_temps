2fcfa297a3f908af33dfb76c90c31220679a5e11
sandrine-proenca <sandrine.proenca@sncf.fr>
2023-04-30 19:35:49 +0200

    fix create et select album

--- src/components/Album/createAlbum.tsx
+++ src/components/Album/createAlbum.tsx
@@ -1,78 +1,78 @@
 import { useContext, useState } from "react";
-import { TAlbums } from "../../Types/TAlbum";
+import { TAlbum } from "../../Types/TAlbum";
 import { UserContext } from "../../context/userContext";
 import { BASE_URL } from "../../constant/base_url";
-import { updateAlbum } from "../../constant/DefaultAlbum";
+import { UPDATE_ALBUM } from "../../constant/DefaultAlbum";
 import '../Accueil/accueil.css';
 
-export default function CreateAlbum(props: {
+
+
+export default function Album(props: {
     setPage: React.Dispatch<React.SetStateAction<string>>;
 })
 {
     const { user, onUserChange } = useContext(UserContext);
-
-    const token = user.access_token;
-
-    const [ album, setAlbum ] = useState(updateAlbum);
+    const [ albums, setAlbums ] = useState(UPDATE_ALBUM);
 
     const inputChange = (e: React.BaseSyntheticEvent) =>
     {
         const { name } = e.target;
-        setAlbum((newAlbum) =>
+        setAlbums((newAlbum) =>
         {
-            return { ...newAlbum, [ name ]: e.target.value }
+            return { ...newAlbum, [ name ]: e.target.value };
         });
     };
 
-
     const addAlbum = (e: React.BaseSyntheticEvent) =>
     {
-        console.log(album);
-
         const options = {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                Authorization: `Bearer ${token}`
+                Authorization: `Bearer ${user.access_token}`,
             },
-            body: JSON.stringify(album),
+            body: JSON.stringify(albums),
         };
-
         fetch(`${BASE_URL}albums`, options)
             .then((response) => response.json())
-            /* .then((donnee) => addAlbumToUser(donnee.data)) */
+            .then((response) =>
+                response.statusCode === 409
+                    ? alert(response.message)
+                    : addAlbumToUser(response.data),
+            )
+
             .catch((err) => console.error(err));
-    }
+    };
 
-    /* const addAlbumToUser = (value: TAlbums) =>
+    const addAlbumToUser = (value: TAlbum) =>
     {
-        console.log(value);
-
-        const newModif = { ...user ,album:[ ...user.album, value ]};
+        const newModif = { ...user };
+        newModif.albums = [ ...newModif.albums, value ];
         onUserChange(newModif);
+    };
 
-    } */
-    
+
 
     return (
         <div className="createAlbum">
             <div className="container  text-center">
-                <div className="position-relative">
-                    <button type="button" className="btn-close position-absolute top-0 end-0 text-white" onClick={() => props.setPage(`accueil`)}></button>
-                    <h3 className='fs-1 text-center text-white'>Créez votre album !</h3>
-                    <div className=" form-log rounded-5 shadow-5-strong p-5">
-                        <div className="form-outline mb-4 mt-4">
-                            <label className='albumName'>Le nom de votre album :</label>
-                            <input type="text" name="name" placeholder="Nom de l'album"
-                                onChange={(e) => inputChange(e)}></input>
-                        </div>
-                        <div className='row justify-content-evenly text-center align-items-center mt-5'>
-                            <button type="submit" className="btn button btn-color col-lg-5 col-sm-10 btn-sm" onClick={(e) =>
-                            {
-                                addAlbum(e);
-                                props.setPage('compteUser')
-                            }}>Enregister</button>
-                        </div>
+
+                <h3 className='fs-1 text-center text-white'>Créez votre album !</h3>
+                <div className=" form-log rounded-5 shadow-5-strong p-5">
+                    <div className="form-outline mb-4 mt-4">
+                        <label htmlFor='albumName'>Le nom de votre album :</label>
+                        <input type="text" name="name" placeholder="Nom de l'album"
+                            onChange={(e) => inputChange(e)}></input>
+                    </div>
+                    <div className='row justify-content-evenly text-center align-items-center mt-5'>
+                        <button type="submit" className="btn button btn-color col-lg-5 col-sm-10 btn-sm" onClick={(e) =>
+                        {
+                            addAlbum(e);
+                            props.setPage('selectAlbums')
+                        }}>Enregister</button>
+
+                        <button type="button" className="btn button btn-color col-lg-5 col-sm-10 btn-sm" onClick={() => props
+                            .setPage(`compteUser`)}>Retour</button>
                     </div>
                 </div>
             </div>
--- src/components/Album/selectAlbums.tsx
+++ src/components/Album/selectAlbums.tsx
@@ -1,64 +1,43 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { UserContext } from "../../context/userContext";
-import { BASE_URL } from "../../constant/base_url";
+import { AlbumContext } from "../../context/albumContext";
 
 export default function SelectAlbums(props: {
     setPage: React.Dispatch<React.SetStateAction<string>>;
 })
 {
     const { user } = useContext(UserContext);
-    const token = user.access_token;
-    
-    const [ albums, setAlbums] = useState(user.album);
 
+    const { setAlbum } = useContext(AlbumContext);
 
-    async function getAllAlbumsOfUser()
-    {
-        const options = {
-            method: 'GET',
-            headers: { 
-                Authorization: `Bearer ${token}` }
-        }
+    const getAllAlbumsOfUser = user.albums?.map((itemAlbum, i) => (
+        <div key={i} className="albumName">
+            <h5 onClick={() => { setAlbum(itemAlbum); props.setPage('compteUser') }}> {itemAlbum.name}</h5>
+                <button type="button" className="btn button btn-color col-lg-5 col-sm-3 btn-sm" onClick={() => props.setPage(`compteUser`)}>Supprimer</button>
+                <button type="button" className="btn button btn-color col-lg-5 col-sm-3 btn-sm" onClick={() => props.setPage(`compteUser`)}>Modifier</button>
+        </div>
 
-        const response = await fetch(`${BASE_URL}albums`, options);
-        const responseJson = await response.json();
-        setAlbums(responseJson.data);
-    }
-
-    useEffect( () => {
-        getAllAlbumsOfUser();
-    }, []);
-
-    /* const albums = user.album?.map((data, i) => (
-            <div className="albumName">
-                <h5 key={i}> {data.name}</h5>
-                <p> {data.id}</p>
-            </div>
-    )); */
-
-
-    
-    console.log();
+    ));
 
     return (
         <div className="selectAlbums">
             <div className="container  text-center">
-                <div className="position-relative">
-                    <button type="button" className="btn-close position-absolute top-0 end-0 text-white" onClick={() => props.setPage(`compteUser`)}></button>
-                </div>
-                {album}</div>
-            <div className=" form-log rounded-5 shadow-5p-5">
+                <div className=" form-log rounded-5 shadow-5p-5">
+
+                    <h3>
+                        {getAllAlbumsOfUser}
+                    </h3>
+
                     <div className='row justify-content-evenly text-center align-items-center mt-5'>
 
-                        <button type="button" className="btn button btn-color col-lg-5 col-sm-3 btn-sm" onClick={() => props.setPage(``)}>
-                            Modifier mon album
+                        <button type="button" className="btn button btn-color col-lg-5 col-sm-3 btn-sm" onClick={() => props.setPage(`compteUser`)}>
+                            Mon compte
                         </button>
 
-                        <button type="button" className="btn button btn-color col-lg-5 col-sm-3 btn-sm" onClick={() => props.setPage(``)}>
-                            Supprimer mon album
-                        </button>
                     </div>
+
                 </div>
+            </div>
         </div>
     );
 }
